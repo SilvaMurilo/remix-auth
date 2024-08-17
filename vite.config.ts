31f64c7ca0c33ebe7493ffa9593c8e232ecc4a57
Murilo Silva <murilo.silva@owempay.com.br>
2024-08-16 22:51:43 -0300

    new test

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-  build: {
-    outDir: "public", // Ajuste conforme necessário
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'app'),
+    },
   },
 });
